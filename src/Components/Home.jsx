2e9790cff4c67be8d02d866739c454bc0e13e763
arjunn2313 <arjunnks123@gmail.com>
2024-05-29 17:06:13 +0530

    navbar updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,37 +1,30 @@
-import React from 'react';
-import Navbar from './Navbar';
-import Navigation from './Navigation';
-import Properties from './Properties';
-import Exclusive from './Exclusive';
-import Real from './Real';
-import Enquiry from './Enquiry';
-import Footer from './Footer';
+import React from "react";
+import Navbar from "./Navbar";
+import Navigation from "./Navigation";
+import Properties from "./Properties";
+import Exclusive from "./Exclusive";
+import Real from "./Real";
+import Enquiry from "./Enquiry";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
-  <>
+    <>
       <Navbar />
-     
-      
-          <Navigation />
-     
-      
-       
-          <Properties />
-     
-  
-      
-          <Exclusive />
- 
-          <Real />
-  
-          <Enquiry />
-     
-    
-          <Footer />
-     
-          </>
+
+      <Navigation />
+
+      <Properties />
+
+      <Exclusive />
+
+      <Real />
+
+      <Enquiry />
+
+      <Footer />
+    </>
   );
-}
+};
 
 export default Home;
