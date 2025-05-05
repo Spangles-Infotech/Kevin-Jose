b9329b9915326b4db4843227ccd2ddb6dba045b6
gautam Wise <gautamwise.s@gmail.com>
2025-05-05 15:11:29 +0530

    Update request.js

--- src/Components/request.js
+++ src/Components/request.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // Update the Baseurl variable to match your Django backend server's URL
-export const Baseurl = "https://kjback.spanglesinfotech.in/api/";
+// export const Baseurl = "https://kjback.spanglesinfotech.in/api/";
+export const Baseurl = "https://realestate-w6bl.onrender.com/api/";
+
 const getTokenFromLocalStorage = localStorage.getItem("customer");
 axios.defaults.withCredentials = true;
 
